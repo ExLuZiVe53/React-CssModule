9dbe065d4c59212be329eae7565562a4ec59a2a6
ExLuZiVe53 <imstudent27@gmail.com>
2024-03-07 22:19:50 +0100

    corect wihout mistakes

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -6,12 +6,13 @@ import {
   FaClock,
 } from "react-icons/fa";
 import css from "components/EventCard/EventCard.module.css";
-// import { formatEventStart, formatEventDuration } from "helpers";
+import { formatEventStart, formatEventDuration } from "helpers";
 import { iconSize } from "constans";
 
 export const EventCard = ({ name, location, speaker, type, start, end }) => {
-  // const formattedStart = formatEventStart(start);
-  // const duration = formatEventDuration(start, end);
+  const formattedStart = formatEventStart(start);
+
+  const duration = formatEventDuration(start, end);
   return (
     <div className={css.event}>
       <h2 className={css.title}>{name}</h2>
@@ -25,11 +26,11 @@ export const EventCard = ({ name, location, speaker, type, start, end }) => {
       </p>
       <p className={css.info}>
         <FaCalendarAlt className={css.icon} size={iconSize.sm} />
-        {/* {formattedStart} */}
+        {formattedStart}
       </p>
       <p className={css.info}>
         <FaClock className={css.icon} size={iconSize.sm} />
-        {/* {duration} */}
+        {duration}
       </p>
       <span className={`${css.chip} ${css[type]}`}>{type}</span>
       {/* free|paid|vip */}
--- src/helpers/formatEventDuration.js
+++ src/helpers/formatEventDuration.js
@@ -1,5 +1,5 @@
-// import { formatDistanceStrict } from "date-fns";
+import { formatDistanceStrict } from "date-fns";
 
-// export const formatEventDuration = (start, end) => {
-//   return formatDistanceStrict(Date.parse(start), Date.parse(end));
-// };
+export const formatEventDuration = (start, end) => {
+  return formatDistanceStrict(Date.parse(start), Date.parse(end));
+};
